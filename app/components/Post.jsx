e9356ea238fd2c1d94e9202170c5b8eaa1196db0
alexKwonIsAwesome <khwalex@gmail.com>
2016-12-01 18:59:43 +0900

    init Post component

---
+++ app/components/Post.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Post = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Single Post
+      </div>
+    )
+  }
+});
+
+module.exports = Post;
