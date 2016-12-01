05550907794c2dc3eb6f54ebdb5d0e1890795dfc
alexKwonIsAwesome <khwalex@gmail.com>
2016-12-01 18:58:59 +0900

    update PostList: created dummy data
    Just for testing purpose, very simple post data is created as status.

--- app/components/PostList.jsx
+++ app/components/PostList.jsx
@@ -1,6 +1,25 @@
 var React = require('react');
 
 var PostList = React.createClass({
+  getInitialState: function () {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: '10 methods to win in gambling',
+          content: 'Did you know there are lots of lovely blablablabla aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+        }, {
+          id: 2, 
+          title: 'How to live life much better',
+          content: "Go to Korea, then your life would be much better. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        }, {
+          id: 3,
+          title: 'Why don\'t you quit your job?',
+          content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        }
+      ]
+    }
+  },
   render: function () {
     return (
       <div>
